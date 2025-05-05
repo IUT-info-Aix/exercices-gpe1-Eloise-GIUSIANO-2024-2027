b705436af494cf2be7a7b95c95caa6b2668bcca4
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-05-05 09:01:04 +0200

    ajout q4 exo 5

--- src/main/java/fr/amu/iut/exercice5/JeuMain.java
+++ src/main/java/fr/amu/iut/exercice5/JeuMain.java
@@ -26,8 +26,11 @@ public class JeuMain extends Application {
         //Acteurs du jeu
         Personnage pacman = new Pacman();
         Personnage fantome = new Fantome();
+
         // on positionne le fantôme 20 positions vers la droite
         fantome.setLayoutX(20 * 10);
+
+
         //panneau du jeu
         Pane jeu = new Pane();
         jeu.setPrefSize(640, 480);
@@ -73,6 +76,11 @@ public class JeuMain extends Application {
      */
     private void deplacer(Personnage j1, Personnage j2) {
         scene.setOnKeyPressed((KeyEvent event) -> {
+            double prevXJ1 = j1.getLayoutX();
+            double prevYJ1 = j1.getLayoutY();
+            double prevXJ2 = j2.getLayoutX();
+            double prevYJ2 = j2.getLayoutY();
+
             switch (event.getCode()) {
                 case LEFT:
                     j1.deplacerAGauche();
@@ -100,8 +108,34 @@ public class JeuMain extends Application {
                     break;
 
             }
+// Collision et repositionnement (séparé du déplacement)
+            boolean collisionJ1 = false;
+            for (Obstacle obstacle : obstacles) {
+                if (j1.getBoundsInParent().intersects(obstacle.getBoundsInParent())) {
+                    collisionJ1 = true;
+                    break;
+                }
+            }
+            if (collisionJ1) {
+                j1.setLayoutX(prevXJ1);
+                j1.setLayoutY(prevYJ1);
+            }
+
+            boolean collisionJ2 = false;
+            for (Obstacle obstacle : obstacles) {
+                if (j2.getBoundsInParent().intersects(obstacle.getBoundsInParent())) {
+                    collisionJ2 = true;
+                    break;
+                }
+            }
+            if (collisionJ2) {
+                j2.setLayoutX(prevXJ2);
+                j2.setLayoutY(prevYJ2);
+            }
+
             if (j1.estEnCollision(j2))
                 primaryStage.close();
         });
     }
 }
+
