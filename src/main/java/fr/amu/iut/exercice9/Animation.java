7ec8c15ba485e4222e3a7f1743e8b9739e18b562
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-05-06 17:09:36 +0200

    exercice 9

--- src/main/java/fr/amu/iut/exercice9/Animation.java
+++ src/main/java/fr/amu/iut/exercice9/Animation.java
@@ -1,5 +1,6 @@
 package fr.amu.iut.exercice9;
 
+import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -18,14 +19,35 @@ public class Animation extends Application {
 
         Duration duration = Duration.millis(1500);
         TranslateTransition transition1 = new TranslateTransition(duration, customButton);
-        transition1.setByX(150);
         transition1.setByY(-150);
-        transition1.setAutoReverse(true);
-        transition1.setCycleCount(2);
+        TranslateTransition transition2 = new TranslateTransition(duration, customButton);
+        transition2.setByX(150);
+        TranslateTransition transition3 = new TranslateTransition(duration, customButton);
+        transition3.setByY(300);
+        TranslateTransition transition4 = new TranslateTransition(duration, customButton);
+        transition4.setByX(-300);
+        TranslateTransition transition5 = new TranslateTransition(duration, customButton);
+        transition5.setByY(-300);
+        TranslateTransition transition11 = new TranslateTransition(duration, customButton);
+        transition11.setByX(150);
 
-//        SequentialTransition st = new SequentialTransition(transition1, transition2, transition3, transition4, transition5);
+        TranslateTransition transition6 = new TranslateTransition(duration, customButton);
+        transition6.setByY(300);
+        TranslateTransition transition7 = new TranslateTransition(duration, customButton);
+        transition7.setByX(300);
+        TranslateTransition transition8 = new TranslateTransition(duration, customButton);
+        transition8.setByY(-300);
+        TranslateTransition transition9 = new TranslateTransition(duration, customButton);
+        transition9.setByX(-150);
+        TranslateTransition transition10 = new TranslateTransition(duration, customButton);
+        transition10.setByY(150);
+        TranslateTransition transition12 = new TranslateTransition(duration, customButton);
+        transition12.setByX(-150);
 
-        customButton.setOnMousePressed(mouseEvent -> transition1.play());
+        SequentialTransition st = new SequentialTransition(transition1, transition2, transition3, transition4, transition5, transition11,  transition12, transition6, transition7, transition8, transition9, transition10);
+        st.play();
+
+        customButton.setOnMousePressed(mouseEvent -> transition6.play());
 
         primaryStage.setTitle("Animation");
         primaryStage.setScene(scene);
