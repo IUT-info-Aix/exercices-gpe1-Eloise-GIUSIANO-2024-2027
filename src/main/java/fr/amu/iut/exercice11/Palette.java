e948f27ea50a09218c8b5ddc66dd1d851c88692d
Eloïse-GIUSIANO[2024-2027] <eloise.giusiano@etu.univ-amu.fr>
2025-05-13 17:25:57 +0200

    fin scéance du 13 mai

--- src/main/java/fr/amu/iut/exercice11/Palette.java
+++ src/main/java/fr/amu/iut/exercice11/Palette.java
@@ -1,6 +1,8 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,12 +12,15 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 @SuppressWarnings("Duplicates")
 public class Palette extends Application {
+    private IntegerProperty nbFois = new SimpleIntegerProperty(0);
+    private String couleurChoisie = "";
 
     private int nbVert = 0;
     private int nbRouge = 0;
@@ -32,7 +37,6 @@ public class Palette extends Application {
     private HBox boutons;
 
     private Label texteDuBas;
-
 
     @Override
     public void start(Stage primaryStage) {
@@ -54,10 +58,37 @@ public class Palette extends Application {
         bas.getChildren().addAll(boutons, texteDuBas);
 
         vert = new Button("Vert");
+
         rouge = new Button("Rouge");
         bleu = new Button("Bleu");
 
         /* VOTRE CODE ICI */
+        vert.setOnAction(event -> {
+            nbVert++;
+            nbFois.set(nbVert);
+            couleurChoisie = "Vert";
+            texteDuHaut.setText("Vert choisi " + nbVert + " fois");
+            panneau.setStyle("-fx-background-color: lightgreen;");
+            texteDuBas.setText("Le " + couleurChoisie + " est une jolie couleur !");
+        });
+
+        rouge.setOnAction(event -> {
+            nbRouge++;
+            nbFois.set(nbRouge);
+            couleurChoisie = "Rouge";
+            texteDuHaut.setText("Rouge choisi " + nbRouge + " fois");
+            panneau.setStyle("-fx-background-color: lightcoral;");
+            texteDuBas.setText("Le " + couleurChoisie + " est une jolie couleur !");
+        });
+
+        bleu.setOnAction(event -> {
+            nbBleu++;
+            nbFois.set(nbBleu);
+            couleurChoisie = "Bleu";
+            texteDuHaut.setText("Bleu choisi " + nbBleu + " fois");
+            panneau.setStyle("-fx-background-color: lightskyblue;");
+            texteDuBas.setText("Le " + couleurChoisie + " est une jolie couleur !");
+        });
 
         boutons.getChildren().addAll(vert, rouge, bleu);
 
@@ -67,8 +98,8 @@ public class Palette extends Application {
 
         Scene scene = new Scene(root);
 
+        primaryStage.setTitle("Palette de Couleurs");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
-
